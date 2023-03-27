676ee221c33d1afd2d19b7de84bee37f17e81314
Kyrylo Divisienko <racket952@gmail.com>
2023-03-27 08:49:08 +0300

    upd hw2

--- salon/views.py
+++ salon/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from salon.models import Services, Master
+from administrator.models import Schedule
+from datetime import datetime, timedelta
 
 def services_handler(request):
+    today = datetime.today()
+    seven_days = today + timedelta(days=7)
+    sсhedule_on_week = Schedule.objects.filter(date__gte=today, date__lte=seven_days)
+    for g in sсhedule_on_week:
+        b=1
     services = Services.objects.all()
+    datetime.now()
+    for service in services:
+        b=1
+
     return HttpResponse('Services page')
 
 
