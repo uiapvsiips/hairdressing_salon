68a7e8e0df396973ff2e2e17099d9c77b33b2150
Kyrylo <uiapvsiips@gmail.com>
2023-03-27 22:22:33 +0300

    upd hw2

--- salon/views.py
+++ salon/views.py
@@ -5,17 +5,18 @@ from administrator.models import Schedule
 from datetime import datetime, timedelta
 
 def services_handler(request):
-    today = datetime.today()
+    today = datetime.today().date()
     seven_days = today + timedelta(days=7)
-    sсhedule_on_week = Schedule.objects.filter(date__gte=today, date__lte=seven_days)
-    for g in sсhedule_on_week:
-        b=1
-    services = Services.objects.all()
-    datetime.now()
-    for service in services:
-        b=1
 
-    return HttpResponse('Services page')
+    # Get schedule for a week and get unique masters_ids from there
+    sсhedule_on_week = Schedule.objects.filter(date__gte=today, date__lte=seven_days).order_by('date','master_id', 'start_time')
+    masters_for_week = set([schedule.master_id for schedule in sсhedule_on_week])
+
+    # Get services for a week and get unique service_ids from there
+    services_for_week = Services.objects.filter(master__services__master__in=masters_for_week)
+    unique_services_for_week = set([service for service in services_for_week])
+    return render(request, 'services.html', context= {'unique_services_for_week': unique_services_for_week})
+    return HttpResponse(f'Services for a week {unique_services_for_week}')
 
 
 def service_id_handler(request, service_id):
