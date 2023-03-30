91d26de714619da56b2f5485c754ea4477600038
Kyrylo Divisienko <racket952@gmail.com>
2023-03-30 20:49:03 +0300

    add hw3

--- administrator/urls.py
+++ administrator/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('panel/bookings/', administrator.views.bookings),
     path('panel/specialists/', administrator.views.specialists),
     path('panel/specialists/<int:specialist_id>/', administrator.views.specialist_id_handler),
+    path('panel/services/', administrator.views.services_handler),
+    path('panel/services/<int:service_id>/', administrator.views.service_id_handler),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from salon.models import Services, Master
 
 # Create your views here.
 def panel(request):
-    return HttpResponse('Admin panel')
+    return render(request, 'admin_panel.html')
 
 
 def bookings(request):
@@ -11,8 +12,47 @@ def bookings(request):
 
 
 def specialists(request):
-    return HttpResponse('Admin panel->specialists')
+    specialists = Master.objects.all()
+    services = Services.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('specialist_name')
+        phone = request.POST.get('specialist_phone')
+        rank = request.POST.get('specialist_rank')
+        new_specialist = Master(name=name, phone=phone, rank=rank)
+        choosen_services = [service for service in request.POST if service.startswith('service_')]
+        new_specialist.save()
+        new_specialist.services.add(*[int(service.split('_')[1]) for service in choosen_services])
+    return render(request, 'admin_specialists.html', context={'specialists': specialists, 'services': services})
 
 
 def specialist_id_handler(request, specialist_id):
-    return HttpResponse(f'Admin panel->specialists id_handler {specialist_id}')
+    specialist = Master.objects.filter(id=specialist_id).first()
+    ser = specialist.services
+    specialist_services = Services.objects.filter(master__services__in=specialist.services)
+    if request.method == 'POST':
+        specialist.name = request.POST.get('specialist_name')
+        specialist.phone = request.POST.get('specialist_phone')
+        specialist.rank = request.POST.get('specialist_rank')
+        specialist.status = request.POST.get('specialist_status')
+        specialist.save()
+    return render(request, 'admin_specialist.html', context={'specialist': specialist,'specialist_services': specialist_services})
+
+
+def services_handler(request):
+    if request.method == 'POST':
+        service_name = request.POST.get('service_name')
+        price = request.POST.get('price')
+        duration = request.POST.get('duration')
+        new_service = Services(name=service_name, price=price, duration=duration)
+        new_service.save()
+    services = Services.objects.all()
+    return render(request, 'admin_services.html', context={'services': services})
+
+def service_id_handler(request, service_id):
+    service = Services.objects.filter(id=service_id).first()
+    if request.method == 'POST':
+        service.name = request.POST.get('service_name')
+        service.price = request.POST.get('price')
+        service.duration = request.POST.get('duration')
+        service.save()
+    return render(request, 'admin_service.html', context={'service': service})
